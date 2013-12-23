2a73fbdeff1bf838f29f8a6b4c7a5f75ba0ebacb
Kruptein <kruptein@gmail.com>
2013-12-23 17:00:50 +0100

    Changed EUW to lowercase euw

--- lolapi.py
+++ lolapi.py
@@ -2,7 +2,8 @@ import requests
 
 class LolAPI:
 	base = "https://prod.api.pvp.net/api/lol/"
-	def __init__(self, key, region="EUW"):
+	def __init__(self, key, region="euw"):
 		self.key = key
 		self.region = region #default region
 
+	
