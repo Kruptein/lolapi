8efd8b8a91d6993b46366374d5e04da6bc938ad7
Kruptein <kruptein@gmail.com>
2013-12-23 17:10:54 +0100

    Added get_champions

--- lolapi.py
+++ lolapi.py
@@ -6,4 +6,7 @@ class LolAPI:
 		self.key = key
 		self.region = region #default region
 
-	
+	def get_champions(self, region="default", freetoplay=False):
+		if region == "default":
+			region = self.region
+		return requests.get(LolAPI.base + "{}/v1.1/champion?freeToPlay={}&api_key={}".format(region, str(freetoplay).lower(), self.key)).json()
