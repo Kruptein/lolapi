ddd72b28ff6332aa59b4ef81dcc6015536d62f18
Darragh Van Tichelen <darragh.ssa@gmail.com>
2014-08-09 11:23:42 +0200

    Fixed opening the config file when not in the same directory

--- rawpi.py
+++ rawpi.py
@@ -1,10 +1,13 @@
+import os
 import json
 import requests
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 # THIS HANDLES NO ERRORS AND IS JUST A RAW CONNECTION TO THE LOL API;
 # USE THE API MODULE TO GET ERROR FALLBACK METHODS
 
-with open('config', 'r') as f:
+with open(os.path.join(__location__, 'config'), 'r') as f:
     try:
         config = json.load(f)
     except ValueError:
