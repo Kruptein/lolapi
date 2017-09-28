b430c911fb800d37d65c0c9828f0523b20fc519a
darragh <kruptein@gmail.com>
2017-09-28 20:57:42 +0200

    Change the way game queues were stored.

--- lolapi/constants.py
+++ lolapi/constants.py
@@ -11,124 +11,370 @@ SEASONS = {
     'SEASON2017': 9
 }
 
-MATCHMAKING_QUEUES_SIMPLE = {
-    0: 'Custom',
-    2: 'Normal 5v5',
-    4: 'Ranked solo 5v5',
-    6: "Ranked premade 5v5",
-    7: "Coop vs AI",
-    8: "Normal 3v3",
-    9: "Ranked flex 3v3",
-    14: "Normal 5v5",
-    16: "Dominion",
-    17: "Dominion",
-    25: "Dominion Coop vs AI",
-    31: "Coop vs AI intro",
-    32: "Coop vs AI beginner",
-    33: "Coop vs AI intermediate",
-    41: "Ranked team 3v3",
-    42: "Ranked team 5v5",
-    52: "Coop vs AI 3v3",
-    61: "Teambuilder",
-    65: "ARAM",
-    70: "One for All",
-    72: "Snowdown Showdown 1v1",
-    73: "Snowdown Showdown 2v2",
-    75: "Hexakill",
-    76: "URF",
-    78: "One for All Mirror",
-    83: "URF vs AI",
-    91: "Doom Bots I",
-    92: "Doom Bots II",
-    93: "Doom Bots V",
-    96: "Ascension",
-    98: "Hexakill TT",
-    100: "Butcher's Bridge",
-    300: "King Poro",
-    310: "Nemesis",
-    313: "Black Market Brawlers",
-    315: "Nexus Siege",
-    317: "Definitely Not Dominion",
-    318: "ARURF",
-    325: "All Random SR",
-    400: "Normal 5v5",
-    410: "Ranked 5v5",
-    420: "Ranked 5v5 solo",
-    430: "Normal 5v5 flex",
-    440: "Ranked 5v5 flex",
-    450: "ARAM",
-    460: "Normal 3v3",
-    470: "Ranked 3v3 flex",
-    600: "Blood Hunt Assassin",
-    610: "Dark Star",
-    800: "TT Coop vs AI intermediate",
-    810: "TT Coop vs AI intro",
-    820: "TT Coop vs AI beginner",
-    830: "SR Coop vs AI intro",
-    840: "SR Coop vs AI beginner",
-    850: "SR Coop vs AI intermediate",
-    980: "Star Guardian Invasion: normal",
-    990: "Star Guardian Invasion: onslaught"
+MATCHMAKING_QUEUES = {
+    0: {
+        'map': "Summoner's Rift",
+        'description': "Custom games",
+        'short_description': 'Custom',
+        'deprecated': False
+    },
+    2: {
+        'map': "Summoner's Rift",
+        'description': "5v5 Blind Pick games",
+        'short_description': '5v5 Normal Blind',
+        'deprecated': True
+    },
+    4: {
+        'map': "Summoner's Rift",
+        'description': "5v5 Ranked Solo games",
+        'short_description': '5v5 Solo Ranked',
+        'deprecated': True
+    },
+    6: {
+        'map': "Summoner's Rift",
+        'description': "5v5 Ranked premade games",
+        'short_description': '5v5 Premade Ranked',
+        'deprecated': True
+    },
+    7: {
+        'map': "Summoner's Rift",
+        'description': "Co-op vs AI games",
+        'short_description': 'COOP vs AI',
+        'deprecated': True
+    },
+    8: {
+        'map': "Twisted Treeline",
+        'description': "3v3 Normal games",
+        'short_description': '3v3 Normal',
+        'deprecated': True
+    },
+    9: {
+        'map': "Twisted Treeline",
+        'description': "3v3 Ranked Flex games",
+        'short_description': '3v3 Flex Ranked',
+        'deprecated': True
+    },
+    14: {
+        'map': "Summoner's Rift",
+        'description': "5v5 Draft pick games",
+        'short_description': '5v5 Normal Draft',
+        'deprecated': True
+    },
+    16: {
+        'map': "Crystal Scar",
+        'description': "5v5 Dominion Blind Pick games",
+        'short_description': '5v5 Dominion Blind',
+        'deprecated': True
+    },
+    17: {
+        'map': "Crystal Scar",
+        'description': "5v5 Dominion Draft Pick games",
+        'short_description': '5v5 Dominion Draft',
+        'deprecated': True
+    },
+    25: {
+        'map': "Crystal Scar",
+        'description': "Dominion Co-op vs AI games",
+        'short_description': 'Dominion COOP vs AI',
+        'deprecated': True
+    },
+    31: {
+        'map': "Summoner's Rift",
+        'description': "Co-op vs AI Intro Bot games",
+        'short_description': 'COOP vs AI Intro',
+        'deprecated': True
+    },
+    32: {
+        'map': "Summoner's Rift",
+        'description': "Co-op vs AI Beginner Bot games",
+        'short_description': 'COOP vs AI Beginner',
+        'deprecated': True
+    },
+    33: {
+        'map': "Summoner's Rift",
+        'description': "Co-op vs AI Intermediate Bot games",
+        'short_description': 'COOP vs AI Intermediate',
+        'deprecated': True
+    },
+    41: {
+        'map': "Twisted Treeline",
+        'description': "3v3 Ranked Team games",
+        'short_description': '3v3 Ranked Team',
+        'deprecated': True
+    },
+    42: {
+        'map': "Summoner's Rift",
+        'description': "5v5 Ranked Team games",
+        'short_description': '5v5 Ranked Team',
+        'deprecated': True
+    },
+    52: {
+        'map': "Twisted Treeline",
+        'description': "Co-op vs AI games",
+        'short_description': '3v3 COOP vs AI',
+        'deprecated': True
+    },
+    61: {
+        'map': "Summoner's Rift",
+        'description': "5v5 Team Builder games",
+        'short_description': '5v5 Team Builder',
+        'deprecated': True
+    },
+    65: {
+        'map': "Howling Abyss",
+        'description': "5v5 ARAM games",
+        'short_description': 'ARAM',
+        'deprecated': True
+    },
+    70: {
+        'map': "Summoner's Rift",
+        'description': "One for All games",
+        'short_description': 'One for All',
+        'deprecated': False
+    },
+    72: {
+        'map': "Howling Abyss",
+        'description': "1v1 Snowdown Showdown games",
+        'short_description': '1v1 Snowdown Showdown',
+        'deprecated': False
+    },
+    73: {
+        'map': "Howling Abyss",
+        'description': "2v2 Snowdown Showdown games",
+        'short_description': '2v2 Snowdown Showdown',
+        'deprecated': False
+    },
+    75: {
+        'map': "Summoner's Rift",
+        'description': "6v6 Hexakill games",
+        'short_description': 'Hexakill',
+        'deprecated': False
+    },
+    76: {
+        'map': "Summoner's Rift",
+        'description': "Ulta Rapid Fire games",
+        'short_description': 'URF',
+        'deprecated': False
+    },
+    78: {
+        'map': "Summoner's Rift",
+        'description': "Mirrored One for All games",
+        'short_description': 'Mirrored One for All',
+        'deprecated': False
+    },
+    83: {
+        'map': "Summoner's Rift",
+        'description': "Co-op vs AI Ultra Rapid Fire games",
+        'short_description': 'COOP vs AI URF',
+        'deprecated': False
+    },
+    91: {
+        'map': "Summoner's Rift",
+        'description': "Doom Bots Rank 1 games",
+        'short_description': 'Doom bots I',
+        'deprecated': False
+    },
+    92: {
+        'map': "Summoner's Rift",
+        'description': "Doom Bots Rank 2 games",
+        'short_description': 'Doom Bots II',
+        'deprecated': False
+    },
+    93: {
+        'map': "Summoner's Rift",
+        'description': "Doom Bots Rank 5 games",
+        'short_description': 'Doom Bots V',
+        'deprecated': False
+    },
+    96: {
+        'map': "Crystal Scar",
+        'description': "Ascension games",
+        'short_description': 'Ascension',
+        'deprecated': False
+    },
+    98: {
+        'map': "Twisted Treeline",
+        'description': "6v6 Hexakill games",
+        'short_description': 'Hexakill TT',
+        'deprecated': False
+    },
+    100: {
+        'map': "Butcher's Bridge",
+        'description': "5v5 ARAM games",
+        'short_description': 'ARAM',
+        'deprecated': False
+    },
+    300: {
+        'map': "Howling Abyss",
+        'description': "King Poro games",
+        'short_description': 'King Poro',
+        'deprecated': False
+    },
+    310: {
+        'map': "Summoner's Rift",
+        'description': "Nemesis games",
+        'short_description': 'Nemesis',
+        'deprecated': False
+    },
+    313: {
+        'map': "Summoner's Rift",
+        'description': "Black Market Brawlers games",
+        'short_description': 'Black Market Brawlers',
+        'deprecated': False
+    },
+    315: {
+        'map': "Summoner's Rift",
+        'description': "Nexus Siege games",
+        'short_description': 'Nexus Siege',
+        'deprecated': True
+    },
+    317: {
+        'map': "Crystal Scar",
+        'description': "Definitely Not Dominion games",
+        'short_description': 'Definitely Not Dominion',
+        'deprecated': False
+    },
+    318: {
+        'map': "Summoner's Rift",
+        'description': "All Random URF games",
+        'short_description': 'ARURF',
+        'deprecated': False
+    },
+    325: {
+        'map': "Summoner's Rift",
+        'description': "All Random games",
+        'short_description': 'ARAM SR',
+        'deprecated': False
+    },
+    400: {
+        'map': "Summoner's Rift",
+        'description': "5v5 Draft Pick games",
+        'short_description': '5v5 Normal Draft',
+        'deprecated': False
+    },
+    410: {
+        'map': "Summoner's Rift",
+        'description': "5v5 Ranked Draft games",
+        'short_description': '5v5 Ranked Draft',
+        'deprecated': False
+    },
+    420: {
+        'map': "Summoner's Rift",
+        'description': "5v5 Ranked Solo games",
+        'short_description': '5v5 Ranked Solo',
+        'deprecated': False
+    },
+    430: {
+        'map': "Summoner's Rift",
+        'description': "5v5 Blind Pick games",
+        'short_description': '5v5 Normal Blind',
+        'deprecated': False
+    },
+    440: {
+        'map': "Summoner's Rift",
+        'description': "5v5 Ranked Flex games",
+        'short_description': '5v5 Ranked Flex',
+        'deprecated': False
+    },
+    450: {
+        'map': "Howling Abyss",
+        'description': "5v5 ARAM games",
+        'short_description': 'ARAM',
+        'deprecated': False
+    },
+    460: {
+        'map': "Twisted Treeline",
+        'description': "3v3 Blind Pick games",
+        'short_description': '3v3 Normal Blind',
+        'deprecated': False
+    },
+    470: {
+        'map': "Twisted Treeline",
+        'description': "3v3 Ranked Flex games",
+        'short_description': '3v3 Ranked Flex',
+        'deprecated': False
+    },
+    600: {
+        'map': "Summoner's Rift",
+        'description': "Blood Hunt Assassin games",
+        'short_description': 'Blood Hunt Assassin',
+        'deprecated': False
+    },
+    610: {
+        'map': "Cosmic Ruins",
+        'description': "Dark Star games",
+        'short_description': 'Dark Star',
+        'deprecated': False
+    },
+    800: {
+        'map': "Twisted Treeline",
+        'description': "Co-op vs AI Intermediate Bot games",
+        'short_description': 'COOP vs AI Intermediate',
+        'deprecated': False
+    },
+    810: {
+        'map': "Twisted Treeline",
+        'description': "Co-op vs AI Intro Bot games",
+        'short_description': 'COOP vs AI Intro',
+        'deprecated': False
+    },
+    820: {
+        'map': "Twisted Treeline",
+        'description': "Co-op vs AI Beginner Bot games",
+        'short_description': 'COOP vs AI Beginner',
+        'deprecated': False
+    },
+    830: {
+        'map': "Summoner's Rift",
+        'description': "Co-op vs AI Intro Bot games",
+        'short_description': 'COOP vs AI Intro',
+        'deprecated': False
+    },
+    840: {
+        'map': "Summoner's Rift",
+        'description': "Co-op vs AI Beginner Bot games",
+        'short_description': 'COOP vs AI Beginner',
+        'deprecated': False
+    },
+    850: {
+        'map': "Summoner's Rift",
+        'description': "Co-op vs AI Intermediate Bot games",
+        'short_description': 'COOP vs AI Intermediate',
+        'deprecated': False
+    },
+    940: {
+        'map': "Summoner's Rift",
+        'description': "Nexus Siege games",
+        'short_description': 'Nexus Siege',
+        'deprecated': False
+    },
+    980: {
+        'map': "Valoran City Park",
+        'description': "Star Guardian Invasion: Normal games",
+        'short_description': 'Star Guardian Invasion Normal',
+        'deprecated': False
+    },
+    990: {
+        'map': "Valoran City Park",
+        'description': "Star Guardian Invasion: Onslaught games",
+        'short_description': 'Star Guardian Onslaught Normal',
+        'deprecated': False
+    },
 }
 
-MATCHMAKING_QUEUES = {
-    0: 'Custom',
-    2: 'Normal 5v5 blind pick',
-    4: 'Ranked solo 5v5',
-    6: 'Ranked premade 5v5',
-    7: 'Historical coop vs ai',
-    8: 'Normal 3v3',
-    9: 'Ranked flex/premade 3v3',
-    14: 'Normal 5v5 draft pick',
-    16: 'Dominion 5v5 blind pick',
-    17: 'Dominion 5v5 draft pick',
-    25: 'Dominion Coop vs AI',
-    31: 'Coop vs AI intro bot',
-    32: 'Coop vs AI beginner bot',
-    33: 'Historical Coop vs AI intermediate bot',
-    41: 'Ranked team 3v3',
-    42: 'Ranked team 5v5',
-    52: 'Twisted treeline Coop vs AI',
-    61: 'Team builder',
-    65: 'ARAM',
-    70: 'One for All',
-    72: 'Snowdown showdown 1v1',
-    73: 'Snowdown showdown 2v2',
-    75: "Summoner's Rift 6x6 Hexakill",
-    76: "URF",
-    78: "One for All (Mirror)",
-    83: "URF vs AI",
-    91: "Doom bots rank 1",
-    92: "Doom bots rank 2",
-    93: "Doom bots rank 5",
-    96: "Ascension",
-    98: "Twisted Treeline 6x6 Hexakill",
-    100: "Butcher's Bridge",
-    300: "King Poro",
-    310: "Nemesis",
-    313: "Black Market Brawlers",
-    315: "Nexus Siege",
-    317: "Definitely Not Dominion",
-    318: "ARURF",
-    325: "All Random Summoner's Rift",
-    400: "Normal 5v5 draft pick Summoner's Rift",
-    410: "Ranked 5v5 draft pick Summoner's Rift",
-    420: "Ranked 5v5 solo team builder",
-    430: "Normal 5v5 blind pick Summoner's Rift",
-    440: "Ranked 5v5 flex Summoner's Rift",
-    450: "ARAM",
-    460: "Normal 3v3 blind pick Twisted Treeline",
-    470: "Ranked 3v3 flex Twisted Treeline",
-    600: "Blood Hunt Assassin",
-    610: "Dark Star",
-    800: "Twisted Treeline Coop vs AI intermediate bot",
-    810: "Twisted Treeline Coop vs AI intro bot",
-    820: "Twisted Treeline Coop vs AI beginner bot",
-    830: "Summoner's Rift Coop vs AI intro bot",
-    840: "Summoner's Rift Coop vs AI beginner bot",
-    850: "Summoner's Rift Coop vs AI intermediate bot",
-    980: "Star Guardian Invasion: normal",
-    990: "Star Guardian Invasion: onslaught"
+DEPRECATION_MAP = {
+    2: 430,
+    4: 420,
+    7: 850,  # Also 840 to be exact
+    8: 460,
+    9: 470,
+    14: 400,
+    31: 830,
+    32: 840,
+    33: 850,
+    52: 800,
+    65: 450,
+    315: 940
 }
     
 GAMEMODES = {
@@ -148,3 +394,7 @@ GAMEMODES = {
     'URF': 'URF',
     'DOOMBOTSTEEMO': 'Doombots Teemo'
 }
+
+
+def get_live_queue_id(old_queue_id: int):
+    return GAMEMODES[DEPRECATION_MAP.get(old_queue_id, old_queue_id)]
